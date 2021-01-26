91a19b0bd5d2f2557d43657b1cf3bae3cee8f643
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-01-26 13:57:50 -0500

    fixed table error

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,18 @@
 --      * **id**: an auto incrementing int that serves as the primary key.
 --      * **burger_name**: a string.
 --      * **devoured**: a boolean.
+
+-- drop if exists
+DROP DATABASE IF EXISTS burgers_db;
+
+-- make database
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- make table
+CREATE TABLE burgers (
+	id int NOT NULL AUTO_INCREMENT,
+	burger_names varchar(25) NOT NULL,
+	devoured BOOLEAN, 
+	PRIMARY KEY (id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (burger_names) VALUES ('The Black & Bleu');
+INSERT INTO burgers (burger_names) VALUES ('Double Bacon Burger');
+INSERT INTO burgers (burger_names) VALUES ('The Original');
